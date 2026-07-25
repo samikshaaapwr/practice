b9758cc0a99f5759b90344bf45c403567b240ad4
samikshaaapwr <samikshaaapwr7070@gmail.com>
2026-07-25 23:58:03 +0530

    25 july 2026

---
+++ ReverseOrder.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class ReverseOrder {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+
+        String[] words = str.split(" ");
+
+        for (int i = words.length - 1; i >= 0; i--) {
+            System.out.print(words[i] + " ");
+        }
+    }
+}    
+
